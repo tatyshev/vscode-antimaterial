cfa8c48f379ba2a513cf7fe579d382567e1c3513
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2017-06-28 23:56:01 +0600

    solarized theme added

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,6 @@
 const fn = require('./fn');
 const base = fn.getSource('base');
+const solarized = fn.getSource('solarized');
 
 fn.createTheme('Antimaterial.json', base);
+fn.createTheme('Antimaterial Solarized.json', solarized);
---
+++ sources/solarized/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'Antimaterial - Solarized',
+  type: 'dark',
+  colors: require('./colors'),
+  tokenColors: require('./tokens')
+}
