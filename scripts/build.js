1cb22df45f65cc3c64b6503247f8090ef6412630
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-06-17 23:10:37 +0600

    dracula added

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,8 @@
 const fn = require('./fn');
 const base = fn.getSource('base');
 const monokai = fn.getSource('monokai');
+const dracula = fn.getSource('dracula');
 
 fn.createTheme('Antimaterial.json', base);
 fn.createTheme('Antimaterial Monokai.json', monokai);
+fn.createTheme('Antimaterial Dracula.json', dracula);
